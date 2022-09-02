7dacd32186f8d81240a4043b51532b5ea3b0d6d7
Leoziin <lokinho_ddtank@hotmail.com>
2022-09-02 08:30:57 -0300

    :truck:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MyComponent from './components/MyComponent';
+import Title from './components/Title';
 
 function App() {
+
+    const n = 15
+
+    const redTitle = false
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* CSS Global */}
+      <h1>React com CSS</h1>
+
+      {/* CSS de Componente */}
+      <MyComponent/>
+
+      {/* Inline CSS */}
+      <p style={{
+        color: "blue",
+        padding: "15px", 
+        borderTop: "2px solid red"
+      }}>
+        Este elemento foi estilizado INLINE
+      </p>
+
+      {/* CSS Inline Dinâmico */}
+      <h2 style={
+          n < 10 ? ({
+            color: "purple"
+          }):({
+            color: "pink"
+          })
+        }>
+        CSS INLINE DINAMICO
+      </h2>
+      <h2 style={
+          n > 10 ? ({
+            color: "purple"
+          }):({
+            color: "pink"
+          })
+        }>
+        CSS INLINE DINAMICO
+      </h2>
+
+      {/* Classe Dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Este título vai ter classe dinâminca
+      </h2>
+
+      {/* CSS Modules */}
+      <Title />
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS de componente</h1>
+        <p className="mycomp-paragraph">Parágrafo do componente</p>
+    </div>
+  )
+}
+
+export default MyComponent
